4604385f1ebb78f52ecbadcbd5ce56f2211bf556
stonewallstan <stanleyarmstrong31@gmail.com>
2018-04-27 15:57:51 -0700

    feat(index): add hero text and image

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import styles from './index.module.css';
 import logo from '../assets/logo.png';
+import animatedTeam from '../assets/team.gif'
 
 /* eslint-disable react/prop-types, max-len */
 const IndexPage = () => (
   <div>
     <header className={styles.hero}>
-      <a
+      <a className={styles.logoLink}
         href="https://slohacks.com/"
       >
         <img
@@ -18,6 +19,23 @@ const IndexPage = () => (
       <h1 className={styles.h1}>
         SLO Hacks
       </h1>
+      <div className={styles.heroText}>
+        <h2>
+          Hello, we're SLO Hacks!
+        </h2>
+        <p>
+          SLO Hacks is a club at Cal Poly SLO that hosts quarterly hackathons.
+          Hackathons are coding events that range anywhere from 12-36 hours,
+          and give students time to build anything they can think of.
+          Our evenets have reached 300 students in its first year, and we aim
+          to reach much, much more.
+        </p>
+      </div>
+     <img
+        src={animatedTeam}
+        alt="SLO Hacks Team"
+        className={styles.animatedTeam}
+    />
     </header>
   </div>
 );
