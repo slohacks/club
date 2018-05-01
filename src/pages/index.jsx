4497b38135a47ab52628b79b5dfcb4b725498fc8
Justin Mo <jmo02@calpoly.edu>
2018-04-30 17:13:26 -0700

    feat(index): create Events section
    fix #5

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -18,6 +18,37 @@ const IndexPage = () => (
       <h1 className={styles.h1}>
         SLO Hacks
       </h1>
+      <div>
+        <h2>
+          Our Events
+        </h2>
+        <h3>
+          2017 - 2018
+        </h3>
+        <div className={styles.event}>
+          <div>
+            <h2>
+              Local Hack Day
+            </h2>
+          </div>
+          <div>
+            <h2>
+              SLO Hacks
+            </h2>
+          </div>
+          <div>
+            <h2>
+              Firecode
+            </h2>
+          </div>
+        </div>
+        <h3>
+          2018 - 2019
+        </h3>
+        <h3>
+          Stay tuned!
+        </h3>
+      </div>
     </header>
   </div>
 );
