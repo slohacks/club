62facf770ccd5e288eb367e24d140f06b46aabad
Jumer Ryan Caragay <jumer.caragay@gmail.com>
2018-05-01 13:19:19 -0700

    feat(ContactForm.jsx Button.jsx): added disabled prop to button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Button.module.css';
@@ -42,45 +41,3 @@ Button.propTypes = {
 };
 
 export default Button;
-=======
-import React from 'react';
-import PropTypes from 'prop-types';
-import styles from './Button.module.css';
-
-const LinkButton = props => (
-  <a
-    href={props.href}
-    target="_blank"
-    rel="noopener noreferrer"
-  >
-    <button
-      type="button"
-      className={styles.Button}
-    >
-      { props.name }
-    </button>
-  </a>
-);
-
-const NonLinkButton = props => (
-  <button
-    type="submit"
-    className={styles.Button}
-  >
-    { props.name }
-  </button>
-);
-
-const Button = props => (
-  props.href
-    ? <LinkButton name={props.name} href={props.href} />
-    : <NonLinkButton name={props.name} />
-);
-
-Button.propTypes = {
-  name: PropTypes.string.isRequired,
-  href: PropTypes.string,
-};
-
-export default Button;
->>>>>>> bddccb9... feat(Button.jsx): make links optional for the button
