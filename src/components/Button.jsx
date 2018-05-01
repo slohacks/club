b23648091941eab34dd55b5c7cbf79cb10d0f403
Jumer Ryan Caragay <jumer.caragay@gmail.com>
2018-04-30 19:16:56 -0700

    feat(Button.jsx): make links optional for the button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Button.module.css';
 
-const Button = props => (
+const LinkButton = props => (
   <a
     href={props.href}
     target="_blank"
@@ -17,9 +17,24 @@ const Button = props => (
   </a>
 );
 
+const NonLinkButton = props => (
+  <button
+    type="submit"
+    className={styles.Button}
+  >
+    { props.name }
+  </button>
+);
+
+const Button = props => (
+  props.href
+    ? <LinkButton name={props.name} href={props.href} />
+    : <NonLinkButton name={props.name} />
+);
+
 Button.propTypes = {
   name: PropTypes.string.isRequired,
-  href: PropTypes.string.isRequired,
+  href: PropTypes.string,
 };
 
 export default Button;
