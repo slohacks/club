705268cb381f6f4194f3633fa9e43b3f8ec6b452
Stanley Armstrong <stanleyarmstrong31@gmail.com>
2018-05-02 23:34:17 -0700

    feat(pages/index): add hero/above-the-fold content  (#18)
    * feat(assets): add images
    
    * feat(index): add hero text and image
    
    * feat(index): add grid for mobile and desktop
    
    * feat(index): include logo on desktop
    
    * feat(pages/index): fix indentation of image tag
    
    completes #4

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,41 +1,47 @@
 import React from 'react';
 import styles from './index.module.css';
 import logo from '../assets/logo.png';
+import animatedTeam from '../assets/team.gif'
 
 /* eslint-disable react/prop-types, max-len */
 const IndexPage = () => (
   <div>
     <header className={styles.hero}>
-      <a
-        href="https://slohacks.com/"
-      >
-        <img
-          src={logo}
-          alt="Logo"
-          className={styles.logo}
-        />
-      </a>
-      <h1 className={styles.h1}>
-        SLO Hacks
-      </h1>
-      <div>
-        <h2>Our Events</h2>
-        <h3>2017 - 2018</h3>
-        <div className={styles.event}>
-          <div>
-            <h2>Local Hack Day</h2>
-          </div>
-          <div>
-            <h2>SLO Hacks</h2>
-          </div>
-          <div>
-            <h2>Firecode</h2>
-          </div>
-        </div>
-        <h3>2018 - 2019</h3>
-        <h3>Stay tuned!</h3>
+      <div className={styles.heroText}>
+        <h2>
+          Hello, we're SLO Hacks!
+        </h2>
+        <p>
+          SLO Hacks is a club at Cal Poly SLO that hosts quarterly hackathons.
+          Hackathons are coding events that range anywhere from 12-36 hours,
+          and give students time to build anything they can think of.
+          Our evenets have reached 300 students in its first year, and we aim
+          to reach much, much more.
+        </p>
       </div>
-    </header>
+      <img
+         src={animatedTeam}
+         alt="SLO Hacks Team"
+         className={styles.animatedTeam}
+     />
+     </header>
+    <div>
+      <h2>Our Events</h2>
+      <h3>2017 - 2018</h3>
+      <div className={styles.event}>
+        <div>
+          <h2>Local Hack Day</h2>
+        </div>
+        <div>
+          <h2>SLO Hacks</h2>
+        </div>
+        <div>
+          <h2>Firecode</h2>
+        </div>
+      </div>
+      <h3>2018 - 2019</h3>
+      <h3>Stay tuned!</h3>
+    </div>
     <div className={styles.partners}>
       <h2 className={styles.h2}>
         Our Partners
