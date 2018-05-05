c122c1dcc8b3ec7273d3db79548f83efe115a2dc
Mian Uddin <mianuddin@gmail.com>
2018-05-04 19:11:23 -0700

    feat(layouts/index): add smooth scroll polyfill
    completes #30

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -1,30 +1,39 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
+import smoothscroll from 'smoothscroll-polyfill';
 import './index.css';
 import styles from './index.module.css';
 
-const TemplateWrapper = ({ children }) => (
-  <div>
-    <Helmet
-      title="SLO Hacks"
-      meta={[
-        { name: 'description', content: 'SLO Hacks' },
-        { name: 'keywords', content: 'san luis obispo, cal poly, hackathon, coding, software, competition' },
-        { name: 'theme-color', content: '#FFFFFF' },
-      ]}
-    />
-    <div>
-      {children()}
-    </div>
-    <footer
-      className={styles.footer}
-    >
-      <p>Made with <span>♥</span> by the SLO Hacks Team</p>
-      <p>© SLO Hacks 2018</p>
-    </footer>
-  </div>
-);
+class TemplateWrapper extends React.Component {
+  componentDidMount() {
+    smoothscroll.polyfill();
+  }
+
+  render() {
+    return (
+      <div>
+        <Helmet
+          title="SLO Hacks"
+          meta={[
+            { name: 'description', content: 'SLO Hacks' },
+            { name: 'keywords', content: 'san luis obispo, cal poly, hackathon, coding, software, competition' },
+            { name: 'theme-color', content: '#FFFFFF' },
+          ]}
+        />
+        <div>
+          {this.props.children()}
+        </div>
+        <footer
+          className={styles.footer}
+        >
+          <p>Made with <span>♥</span> by the SLO Hacks Team</p>
+          <p>© SLO Hacks 2018</p>
+        </footer>
+      </div>
+    );
+  }
+}
 
 TemplateWrapper.propTypes = {
   children: PropTypes.func, // eslint-disable-line react/require-default-props
